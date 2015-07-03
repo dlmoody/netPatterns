08d96879ef8008ccd1c60224b9e42daa05b4e86d
David Moody <moody.david@gmail.com>
2015-07-03 19:42:43 -0400

    try again

--- build.sh
+++ build.sh
@@ -1,38 +1,23 @@
 #!/usr/bin/env bash
 
-if test `uname` = Darwin; then
-    cachedir=~/Library/Caches/KBuild
-else
-    if [ -z $XDG_DATA_HOME ]; then
-        cachedir=$HOME/.local/share
-    else
-        cachedir=$XDG_DATA_HOME;
-    fi
-fi
-mkdir -p $cachedir
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+sudo apt-get update
+sudo apt-get install Mono-Complete
 
-url=https://www.nuget.org/nuget.exe
+sudo apt-get install automake libtool curl
+curl -sSL https://github.com/libuv/libuv/archive/v1.4.2.tar.gz | sudo tar zxfv - -C /usr/local/src
+cd /usr/local/src/libuv-1.4.2
+sudo sh autogen.sha
+sudo ./configure
+sudo make
+sudo make install
+sudo rm -rf /usr/local/src/libuv-1.4.2 && cd ~/
+sudo ldconfig
 
-if test ! -f $cachedir/nuget.exe; then
-    wget -O $cachedir/nuget.exe $url 2>/dev/null || curl -o $cachedir/nuget.exe --location $url /dev/null
-fi
+curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh && source ~/.dnx/dnvm/dnvm.s
+source /home/parallels/.dnx/dnvm/dnvm.sh
+dnvm
+dnvm upgrade
 
-if test ! -e .nuget; then
-    mkdir .nuget
-    cp $cachedir/nuget.exe .nuget/nuget.exe
-fi
-
-if test ! -d packages/KoreBuild; then
-    mono .nuget/nuget.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
-    mono .nuget/nuget.exe install Sake -version 0.2 -o packages -ExcludeVersion
-fi
-
-if ! type dnvm > /dev/null 2>&1; then
-    source packages/KoreBuild/build/dnvm.sh
-fi
-
-if ! type dnx > /dev/null 2>&1; then
-    dnvm upgrade
-fi
-
-mono packages/Sake/tools/Sake.exe -I packages/KoreBuild/build -f makefile.shade "$@"
+dnu build
